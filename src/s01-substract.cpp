7cb52a2d63f4a45b3e11d974e7641bb172af11ed
BartoszNitschke <92035419+BartoszNitschke@users.noreply.github.com>
2021-11-12 15:03:03 +0100

    Odejmowanie

---
+++ src/s01-substract.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int a, b,roznica;
+
+
+int main()
+{
+    cout << "Podaj pierwsza liczbe: " << endl;
+    cin >> a;
+    cout << "Podaj druga liczbe: " << endl;
+    cin >> b;
+
+    roznica = a - b;
+
+    cout << "Roznica to: " << roznica << endl;
+    
+    
+    
+    return 0;
+}
