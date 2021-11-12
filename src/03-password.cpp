6ffc74881c534a60a900ba1b1ad758ae815d51d9
BartoszNitschke <92035419+BartoszNitschke@users.noreply.github.com>
2021-11-12 16:18:40 +0100

    Update 03-password.cpp

--- src/03-password.cpp
+++ src/03-password.cpp
@@ -6,14 +6,14 @@ std::string haslo;
 std::string haslo2;
 int main()
 {
-std::cout<<"Proszę podać hasło :";
+std::cout<<"Proszę podać haslo :";
 std::cin>>haslo;
 do
 {
-std::cout<<"Podaj ponownie hasło :";
+std::cout<<"Podaj ponownie haslo :";
 std::cin>> haslo2;
 }while(haslo != haslo2);
-std::cout<<"Dobre hasło"<<std::endl;
+std::cout<<"Dobre haslo"<<std::endl;
 }
 
 
