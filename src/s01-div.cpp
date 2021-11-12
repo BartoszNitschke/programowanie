44e96bf11978ea632ffbf34059aaf46e59301b4a
BartoszNitschke <92035419+BartoszNitschke@users.noreply.github.com>
2021-11-12 15:04:10 +0100

    dzielenie

---
+++ src/s01-div.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int a, b,iloraz;
+
+
+int main()
+{
+    cout << "Podaj pierwsza liczbe: " << endl;
+    cin >> a;
+    cout << "Podaj druga liczbe: " << endl;
+    cin >> b;
+
+    Iloraz = a / b;
+
+    cout << "Iloraz to: " << suma << endl;
+    
+    
+    
+    return 0;
+}
