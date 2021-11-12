0cdbf17dca3b4e20430c456b84cc4b3baa663987
BartoszNitschke <92035419+BartoszNitschke@users.noreply.github.com>
2021-11-12 15:02:23 +0100

    dodawanie

---
+++ src/s01-add.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int a, b,suma;
+
+
+int main()
+{
+    cout << "Podaj pierwsza liczbe: " << endl;
+    cin >> a;
+    cout << "Podaj druga liczbe: " << endl;
+    cin >> b;
+
+    suma = a + b;
+
+    cout << "Suma to: " << suma << endl;
+    
+    
+    
+    return 0;
+}
