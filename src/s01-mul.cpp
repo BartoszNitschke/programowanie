527992f9cb7f33c5edf89137e96ee041e103aed6
BartoszNitschke <92035419+BartoszNitschke@users.noreply.github.com>
2021-11-12 15:03:42 +0100

    mnozenie

---
+++ src/s01-mul.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int a, b,iloczyn;
+
+
+int main()
+{
+    cout << "Podaj pierwsza liczbe: " << endl;
+    cin >> a;
+    cout << "Podaj druga liczbe: " << endl;
+    cin >> b;
+
+    Iloczyn = a * b;
+
+    cout << "Iloczyn to: " << suma << endl;
+    
+    
+    
+    return 0;
+}
