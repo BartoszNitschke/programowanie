6df3378e45b51c4295b88188ce3508c6147a6de3
BartoszNitschke <92035419+BartoszNitschke@users.noreply.github.com>
2022-01-02 03:50:51 +0100

    zadania dodatkowe

---
+++ lpierwsza.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int liczba,dzielnik = 0,licznik=0;
+
+    cout << "Wprowadz liczbe: ";
+    cin >> liczba;
+
+   
+    for (int i = 1; i <= liczba; i++)
+    {
+        if ((liczba % i) == 0)
+        {
+            dzielnik++;
+           
+        }
+        licznik++;
+    }
+
+    if (dzielnik == 2)
+        cout << liczba << " jest pierwsza" << endl;
+    else
+        cout << liczba << " nie jest pierwsza" << endl;
+
+    cout << "Petle wykonano " << licznik << " razy" << endl;
+
+    return 0;
+}
